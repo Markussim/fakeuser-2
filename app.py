e851764e65ce6651636ad0ffc5c54408ad674eaf
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2023-11-08 13:26:13 +0100

    Init

---
+++ app.py
@@ -0,0 +1,90 @@
+from dotenv import load_dotenv
+import os
+import random
+import discord
+from pprint import pprint
+from openai import OpenAI
+
+# Load the .env file
+load_dotenv()
+
+# Constants
+openai_key = os.getenv("OPENAI_API_KEY")
+discord_key = os.getenv("DISCORD_BOT_TOKEN")
+BACKSTORY_FILE = "backstory.txt"
+CHANCE_TO_RESPOND = 1  # 1 in 10 chance to respond
+OPENAI_MODEL = "gpt-4-vision-preview"
+MAX_TOKENS = 300
+MESSAGE_HISTORY_LIMIT = 5
+
+# Initialize OpenAI
+openaiClient = OpenAI(api_key=openai_key)
+
+# Read backstory.txt
+with open(BACKSTORY_FILE, "r") as file:
+    backstory = file.read()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged on as {self.user}!")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        random_number = random.randint(1, 10)
+
+        # Check if bot is mentioned
+        if self.user.mentioned_in(message) or random_number <= CHANCE_TO_RESPOND:
+            # Send typing indicator
+            await message.channel.typing()
+
+            response = openaiClient.chat.completions.create(
+                model=OPENAI_MODEL,
+                messages=await self.construct_content_list(message),
+                max_tokens=MAX_TOKENS,
+            )
+
+            print(response.choices[0].message.content)
+
+            await message.channel.send(response.choices[0].message.content)
+
+    async def construct_content_list(self, message):
+        # First message is the backstory with the role 'system'
+        content_list = [{"role": "system", "content": backstory}]
+
+        # Temporarily store messages before reversing
+        message_history = []
+
+        # Fetch the last 5 messages and append them to message_history
+        async for past_message in message.channel.history(limit=MESSAGE_HISTORY_LIMIT):
+            if past_message.author == self.user:
+                message_history.append(
+                    {"role": "assistant", "content": past_message.content}
+                )
+            else:
+                user_content = (
+                    f"Message from {past_message.author.name}: \n{past_message.content}"
+                )
+                if past_message.attachments:
+                    user_content += "\n" + "\n".join(
+                        [attachment.url for attachment in past_message.attachments]
+                    )
+                message_history.append({"role": "user", "content": user_content})
+
+        # Reverse the message_history to be in chronological order
+        message_history.reverse()
+
+        # Extend content_list with the reversed message_history, preserving the backstory as the first item
+        content_list.extend(message_history)
+
+        return content_list
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+client.run(discord_key)
